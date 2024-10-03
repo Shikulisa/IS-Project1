ed1d4888fa291a241a978b0c85ceb8c9a4b8b4a4
Shikulisa <lisa.wanjiku@strathmore.edu>
2024-10-03 07:00:14 +0300

    changing the username in db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'CAT1';
-$username = 'app_user'; // Change as needed
-$password = 'your_password'; // Change as needed
+$username = 'root'; // Change as needed
+$password = ''; // Change as needed
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
